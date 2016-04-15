6e72e926ded3b9dd02a83423d0170c15aa43bce9
shawnrdooley <s-dooley@onu.edu>
2016-04-14 20:49:09 -0400

    GUI elements for all 6 tables

--- NASCAR.php
+++ NASCAR.php
@@ -40,13 +40,6 @@ save("username");
 save("password");
 save("database");
 
-
-
-$adatabase = new DatabaseCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"]);
-$resultsxdrivers = $adatabase->complexRead(["LastName","FirstName","Suffix","Place","Name"], ["Drivers", "RaceResults", "Schedule",  "Races"], [new Condition("Drivers.ID","=","DriverID"),new Condition("ScheduleID","=","Schedule.ID"),new Condition("RaceID","=","Races.ID")]);
-echo $resultsxdrivers->getHTMLTable();
-
-
 echo "<form action='' method='post'>";
 $s = new SchemaCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"]);
 $tb = new GUItable();
@@ -55,45 +48,108 @@ echo $tb->getListBox("database");
 echo "<input type='submit' value='Select DB'></form>";
 
 
-////add a race result
+
+
+//track type
 echo "<form action='' method='post'>";
-echo "<br><br>Insert a Race Result: <br><br>";
-echo "Race:"; //fk for schedule 1
-$raceresults = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "Schedule");
-$tbSchedule = $raceresults->read();
-echo $tbSchedule->getListBox("sch", 0);
-save("sch");
+echo "Add a Track Type:<br>";
+$trackType = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "TrackTypes");
 
-echo "<br>Driver:"; //fk for driver 1
-$drivers = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "Drivers");
-$tbDrivers = $drivers->read();
-echo $tbDrivers->getListBox("Drivers", 0);
-save("Drivers");
+echo "<br>Name:";
+echo "<input type='text' name='trackTypeName'>";save("trackTypeName");
+echo "<br>Description:";
+echo "<input type='text' name='trackTypeDesc'>";save("trackTypeDesc");
 
+if (isset($_SESSION["trackTypeName"])){
+$trackType->create(["Name","Description"], [$_SESSION["trackTypeName"], $_SESSION["trackTypeDesc"]]);
 
-echo "<br>Place:"; //text box
-echo "<input type='text' name='place'>";
-save("place");
-
-$raceresults = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "RaceResults");
-$tbRaceResults = $raceresults->read();
-echo $tbRaceResults->getHTMLTable();
-
-if (isset($_SESSION["place"])){
-$raceresults->create(["ScheduleID","DriverID","Place"], [$_SESSION["sch"], $_SESSION["Drivers"], $_SESSION["place"]]);
-unset($_SESSION["place"]);
+unset($_SESSION["trackTypeName"]);
 }
-
-
-
-//$q->sendQuery("INSERT INTO `dbShawn`.`RaceResults` (`ScheduleID`, `DriverID`, `Place`) VALUES ('" . $_SESSION["sch"] ."', '" . $_SESSION["Drivers"] . "', '" . $_SESSION["place"]. "');", true);
-
-echo "<input type='submit' value='Insert Race Result'></form>";
+echo "<br><input type='submit' value='Add'></form>";
 
 
 
 
-////add a driver
+//add track
+echo "<form action='' method='post'>";
+echo "Add a Track:<br>";
+$track = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "Tracks");
+
+echo "<br>Name:";
+echo "<input type='text' name='trackName'>";save("trackName");
+echo "<br>City:";
+echo "<input type='text' name='trackCity'>";save("trackCity");
+echo "<br>State:";
+echo "<input type='text' name='trackState'>";save("trackState");
+echo $trackType->read()->getListBox("track_type"); save("track_type");
+echo "<br>Length:";
+echo "<input type='text' name='trackLength'>";save("trackLength");
+echo "<br>Capacity:";
+echo "<input type='text' name='trackCapacity'>";save("trackCapacity");
+
+
+if (isset($_SESSION["trackName"])){
+$track->create(["Name", "City", "State", "TrackType", "Length", "Capacity"],
+ [$_SESSION["trackName"], $_SESSION["trackCity"], $_SESSION["trackState"], $_SESSION["track_type"], $_SESSION["trackLength"], $_SESSION["trackCapacity"]]);
+
+unset($_SESSION["trackName"]);
+}
+echo "<br><input type='submit' value='Add'></form>";
+
+
+
+
+//add race
+echo "<form action='' method='post'>";
+echo "Add a Race:<br>";
+$race = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "Races");
+
+echo "<br>Name:";
+echo "<input type='text' name='raceName'><br>";save("raceName");
+echo $track->read()->getListBox("track_id"); save("track_id");
+echo "<br>Distance:";
+echo "<input type='text' name='raceDist'>";save("raceDist");
+
+
+
+if (isset($_SESSION["raceName"])){
+$race->create(["Name", "TrackID", "Distance"],
+ [$_SESSION["raceName"], $_SESSION["track_id"], $_SESSION["raceDist"]]);
+
+unset($_SESSION["raceName"]);
+}
+echo "<br><input type='submit' value='Add'></form>";
+
+
+
+
+//schedule
+echo "<form action='' method='post'>";
+echo "Add a Schedule:<br>";
+$schedule = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "Schedule");
+
+echo "<br>Year:";
+echo "<input type='text' name='schYear'><br>";save("schYear");
+echo $race->read()->getListBox("race_id"); save("race_id");
+echo "<br>Date:";
+echo "<input type='date' name='schDate'>";save("schDate");
+echo "<br>Time:";
+echo "<input type='time' name='schTime'>";save("schTime");
+
+
+
+if (isset($_SESSION["schYear"])){
+$schedule->create(["Year", "RaceID", "DateOfRace", "TimeOfRace"],
+ [$_SESSION["schYear"], $_SESSION["race_id"], $_SESSION["schDate"], $_SESSION["schTime"]]);
+
+unset($_SESSION["schYear"]);
+}
+echo "<br><input type='submit' value='Add'></form>";
+
+
+
+
+//add a driver
 echo "<form action='' method='post'>";
 
 echo "<br>Last Name:";
@@ -122,6 +178,52 @@ $drivers->create(["LastName", "FirstName", "Suffix", "BirthDate", "City", "State
 unset($_SESSION["lname"]);
 }
 echo "<br><input type='submit' value='Add a Driver'></form>";
+
+
+
+
+//add a race result
+echo "<form action='' method='post'>";
+echo "<br><br>Insert a Race Result: <br><br>";
+echo "Race:"; //fk for schedule 1
+$raceresults = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "Schedule");
+$tbSchedule = $raceresults->read();
+echo $tbSchedule->getListBox("sch", 0);
+save("sch");
+
+echo "<br>Driver:"; //fk for driver 1
+$drivers = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "Drivers");
+$tbDrivers = $drivers->read();
+echo $tbDrivers->getListBox("Drivers", 0);
+save("Drivers");
+
+
+echo "<br>Place:";
+echo "<input type='text' name='place'>";
+save("place");
+
+$raceresults = new TableCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"], "RaceResults");
+$tbRaceResults = $raceresults->read();
+echo $tbRaceResults->getHTMLTable();
+
+if (isset($_SESSION["place"])){
+$raceresults->create(["ScheduleID","DriverID","Place"], [$_SESSION["sch"], $_SESSION["Drivers"], $_SESSION["place"]]);
+unset($_SESSION["place"]);
+}
+
+
+
+//old: $q->sendQuery("INSERT INTO `dbShawn`.`RaceResults` (`ScheduleID`, `DriverID`, `Place`) VALUES ('" . $_SESSION["sch"] ."', '" . $_SESSION["Drivers"] . "', '" . $_SESSION["place"]. "');", true);
+
+echo "<input type='submit' value='Insert Race Result'></form>";
+
+
+
+$adatabase = new DatabaseCRUD($_SESSION["server"], $_SESSION["username"], $_SESSION["password"], $_SESSION["database"]);
+$resultsxdrivers = $adatabase->complexRead(["LastName","FirstName","Suffix","Place","Name"], ["Drivers", "RaceResults", "Schedule",  "Races"], [new Condition("Drivers.ID","=","DriverID"),new Condition("ScheduleID","=","Schedule.ID"),new Condition("RaceID","=","Races.ID")]);
+echo $resultsxdrivers->getHTMLTable();
+
+
 ?>
 
 
